84680fb8f9a209e84d959d87d40d9e22c78c056b
Germán Ramos <german.ramos@lightit.io>
2025-11-13 11:33:02 -0300

    feat: Filters Header desktop view

--- src/components/search-filters/SearchFilters.tsx
+++ src/components/search-filters/SearchFilters.tsx
@@ -13,7 +13,7 @@ const CLINICS = ["Example Clinic", "Example Clinic 1", "Example Clinic 2"];
 
 export const SearchFilters = () => {
   return (
-    <section className="fixed top-0 left-0 w-full max-w-4xl mt-16 mx-auto self-stretch px-6 py-3 inline-flex flex-col justify-start items-start gap-4">
+    <section className="w-full mt-16 self-stretch px-6 lg:px-44 lg:pt-6 lg:pb-3 py-3 inline-flex flex-col justify-start items-start gap-4 lg:gap-7">
       <SearchFiltersHeader />
 
       <div className="self-stretch flex flex-col justify-start items-start gap-5">
