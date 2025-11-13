224af0423cb170487a856fa5f21293e94b5ec3e0
Germán Ramos <german.ramos@lightit.io>
2025-11-13 12:38:41 -0300

    fix: Dropdown options

--- src/components/search-filters/FilterDropdown.tsx
+++ src/components/search-filters/FilterDropdown.tsx
@@ -22,30 +22,30 @@ export const FilterDropdown = ({
   };
 
   return (
-    <div className="self-stretch lg:w-56 inline-flex flex-col justify-start items-start">
-        <div className="self-stretch flex flex-col justify-start items-start gap-2">
-          <div
-            className="self-stretch h-10 bg-(--background-default-default) rounded-md outline-1 -outline-offset-1 outline-(--border-default) inline-flex justify-start items-center overflow-hidden cursor-pointer"
-            onClick={() => setIsOpen((prev) => !prev)}
-          >
-            <div className="flex-1 flex justify-start items-center overflow-hidden">
-              <div className="flex-1 self-stretch px-3 py-1.5 flex justify-between items-center">
-                <span className="flex-1 justify-start text-(--text-default) text-sm font-normal font-[DM_Sans] leading-5 line-clamp-1">
-                  {selected}
-                </span>
-              </div>
+    <div className="self-stretch lg:w-56 inline-flex flex-col justify-start items-start relative">
+      <div className="self-stretch flex flex-col justify-start items-start gap-2">
+        <div
+          className="self-stretch h-10 bg-(--background-default-default) rounded-md outline-1 -outline-offset-1 outline-(--border-default) inline-flex justify-start items-center overflow-hidden cursor-pointer"
+          onClick={() => setIsOpen((prev) => !prev)}
+        >
+          <div className="flex-1 flex justify-start items-center overflow-hidden">
+            <div className="flex-1 self-stretch px-3 py-1.5 flex justify-between items-center">
+              <span className="flex-1 justify-start text-(--text-default) text-sm font-normal font-[DM_Sans] leading-5 line-clamp-1">
+                {selected}
+              </span>
+            </div>
 
-              <div className="self-stretch pr-3 flex justify-start items-center gap-2">
-                <div className="w-4 h-4 relative">
-                  <ChevronDown />
-                </div>
+            <div className="self-stretch pr-3 flex justify-start items-center gap-2">
+              <div className="w-4 h-4 relative">
+                <ChevronDown />
               </div>
             </div>
           </div>
+        </div>
       </div>
 
       {isOpen && (
-        <div className="absolute top-12 left-0 w-full bg-(--background-default-default) border border-(--border-default) rounded-md shadow-md z-20">
+        <div className="top-full mt-1 overflow-y-auto left-0 w-full bg-(--background-default-default) border border-(--border-default) rounded-md shadow-md z-20">
           <button
             className="w-full text-left px-3 py-2 hover:bg-gray-100 cursor-pointer text-sm"
             onClick={() => handleSelect(placeholder)}
