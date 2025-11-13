32759d4377c6e2f93643ccd5fb20f1471840746b
Germán Ramos <german.ramos@lightit.io>
2025-11-13 10:00:07 -0300

    ClinicFilter Component

--- src/components/search-filters/SearchFilters.tsx
+++ src/components/search-filters/SearchFilters.tsx
@@ -3,6 +3,7 @@ import {
   NameFilter,
   SpecialityFilter,
   GenderFilter,
+  ClinicFilter
 } from "..";
 
 export function SearchFilters() {
@@ -18,9 +19,7 @@ export function SearchFilters() {
 
           <GenderFilter />
 
-          <select className="w-full border border-gray-300 rounded-lg px-4 py-2 text-sm text-gray-700 focus:ring-2 focus:ring-blue-500 focus:border-blue-500">
-            <option>All clinics</option>
-          </select>
+          <ClinicFilter />
         </div>
       </div>
     </section>
