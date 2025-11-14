d8b5a8043bdd715cb15e337a4a692d1def494926
Germán Ramos <german.ramos@lightit.io>
2025-11-14 09:58:41 -0300

    fix: Dropdown Options position

--- src/components/search-filters/FilterDropdown.tsx
+++ src/components/search-filters/FilterDropdown.tsx
@@ -45,7 +45,10 @@ export const FilterDropdown = ({
       </div>
 
       {isOpen && (
-        <div className="top-full mt-1 overflow-y-auto left-0 w-full bg-background-surface border border-border-default rounded-md shadow-md z-20">
+        <div
+          className="absolute top-full mt-1 left-0 w-full max-h-60
+            overflow-y-auto bg-background-surface border border-border-default rounded-md shadow-md z-20"
+        >
           <button
             className="w-full text-left px-3 py-2 hover:bg-gray-100 cursor-pointer text-sm"
             onClick={() => handleSelect(placeholder)}
