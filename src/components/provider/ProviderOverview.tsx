d1bfc2997d12a4e048e8082848de8f4f89cf91d0
Germán Ramos <german.ramos@lightit.io>
2025-11-17 14:56:16 -0300

    fix: Languages Conditional

--- src/components/provider/ProviderOverview.tsx
+++ src/components/provider/ProviderOverview.tsx
@@ -18,25 +18,29 @@ export const ProviderOverview = ({
   return (
     <div className="pt-6 space-y-4">
       <ProviderOverviewSection title="About">
-        <p className="text-text-default-secondary font-light">
-          {about}
-        </p>
+        <p className="text-text-default-secondary font-light">{about}</p>
       </ProviderOverviewSection>
 
       <ProviderOverviewSection title="Contact information">
         <div className="pt-1 flex flex-col gap-1">
           <ProviderContactRow icon={<PhoneIcon />} text={phone} />
-          <ProviderContactRow icon={<MailIcon />} text={email}/>
+          <ProviderContactRow icon={<MailIcon />} text={email} />
         </div>
       </ProviderOverviewSection>
 
       <ProviderOverviewSection title="Languages" withDivider={false}>
-        <div className="inline-flex items-center gap-2 pt-2">
-          <WorldIcon />
-          <p className="text-text-default-secondary font-light">
-            {languages.join(", ")}
+        {languages.length > 0 ? (
+          <div className="inline-flex items-center gap-2 pt-2">
+            <WorldIcon />
+            <p className="text-text-default-secondary font-light">
+              {languages.join(", ")}
+            </p>
+          </div>
+        ) : (
+          <p className="text-text-default-secondary font-light pt-2">
+            No language information available
           </p>
-        </div>
+        )}
       </ProviderOverviewSection>
     </div>
   );
