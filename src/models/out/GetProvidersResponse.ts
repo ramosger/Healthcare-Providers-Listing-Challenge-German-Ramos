1849baaab25c8432157bd1c2788815abcb7e44db
Germán Ramos <german.ramos@lightit.io>
2025-11-18 16:42:03 -0300

    refactor: GetProvidersResponse

---
+++ src/models/out/GetProvidersResponse.ts
@@ -0,0 +1,7 @@
+import type { Provider } from "../../domain";
+
+export type GetProvidersResponse = {
+  data: Provider[];
+  links: unknown;
+  meta: unknown;
+};
--- src/services/providerService.ts
+++ src/services/providerService.ts
@@ -1,15 +1,10 @@
 import axios from "axios";
 import type { Provider } from "../domain";
-
-type ProvidersApiResponse = {
-  data: Provider[];
-  links: unknown;
-  meta: unknown;
-};
+import type { GetProvidersResponse } from "../models";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const getProviders = async (): Promise<Provider[]> => {
-  const response = await axios.get<ProvidersApiResponse>(`${API_BASE_URL}/providers`);
+  const response = await axios.get<GetProvidersResponse>(`${API_BASE_URL}/providers`);
   return response.data.data;
 };
